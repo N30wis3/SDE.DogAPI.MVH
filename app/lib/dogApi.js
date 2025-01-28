4038e5304c9b2bb9aa2304095c357302169110e1
N30wis3 <mart030r@edu.sde.dk>
2025-01-28 09:49:50 +0100

    Added sub-breeds to the list of dogs
    Now you can pin point the exact dog you want

--- app/lib/dogApi.js
+++ app/lib/dogApi.js
@@ -3,11 +3,26 @@ const API_BASE = "https://dog.ceo/api";
 export async function fetchBreeds() {
   const res = await fetch(`${API_BASE}/breeds/list/all`);
   const data = await res.json();
-  return Object.keys(data.message); // Returner kun listen af racer
+  const breedsWithSubBreeds = [];
+
+  Object.entries(data.message).forEach(([breed, subBreeds]) => {
+    if (subBreeds.length > 0) {
+      subBreeds.forEach((subBreed) => {
+        breedsWithSubBreeds.push(`${subBreed} ${breed}`);
+      });
+    } else {
+      breedsWithSubBreeds.push(breed);
+    }
+  });
+
+  return breedsWithSubBreeds; // Returner både racer og underacer
 }
 
 export async function fetchBreedImages(breed) {
-  const res = await fetch(`${API_BASE}/breed/${breed}/images`);
+  const breedPath = breed.includes(" ")
+    ? breed.split(" ").reverse().join("/")
+    : breed;
+  const res = await fetch(`${API_BASE}/breed/${breedPath}/images`);
   const data = await res.json();
   return data.message; // Returner listen af billeder
 }
